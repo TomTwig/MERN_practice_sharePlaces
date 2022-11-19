e7c7e70904949f37ae6327f0a38bafcb795dd003
Tom Twig <tom.rosenzweig@live.de>
2022-11-19 12:37:46 +0100

    add Users, NewPlace Routes + fallback Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, Navigate,} from "react-router-dom"
+import NewPlace from './places/pages/NewPlace';
 import Users from './users/pages/Users';
 
 function App() {
@@ -10,8 +11,12 @@ function App() {
   <Routes>
 
     <Route path='/' element={<Users/>} />
+    <Route path='/places/new' element={<NewPlace/>}/>
+    <Route path='*' element={<Navigate to={"/"}/>} />
+    
+    
   </Routes>
-
+  
 
 
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,11 @@
+
+import React from "react"
+
+export default function NewPlace(){
+
+
+    return(
+
+        <h2>New Place Work</h2>
+    )
+}
