3d0798f24df760f14558af0095b0db47b7b79a30
Tom Rosenzweig <tom.rosenzweig@live.de>
2022-11-23 16:48:09 +0100

    add dynamic Link UserItem

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,22 +1,24 @@
 import React from "react";
 import Avatar from "../../shared/components/UIElements/Avatar";
 import "./UserItem.css";
-
+import { Link } from "react-router-dom";
 
 export default function UserItem({ id, image, name, placeCount }) {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <Avatar image= {image} width={"100px"}/>
+      <Link to={`/${id}/places`}>
+        <div className="user-item__content">
+          <div className="user-item__image">
+            <Avatar image={image} width={"100px"} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <p>
+              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+            </p>
+          </div>
         </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <p>
-            {placeCount} {placeCount === 1 ? "Place" : "Places"}
-          </p>
-        </div>
-      </div>
+      </Link>
     </li>
   );
 }
