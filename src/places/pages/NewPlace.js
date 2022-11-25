fab90600ef5fd2709d629e8681de5d4a5f343848
Tom Rosenzweig <tom.rosenzweig@live.de>
2022-11-25 13:03:07 +0100

    add basic custom input element for form at NewPlaces Page

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,15 @@
 
 import React from "react"
+import Input from "../../shared/components/FormElements/Input"
 
 export default function NewPlace(){
 
 
     return(
         <form className="place-form">
+
+            <Input element={"input"} type={"text"} label={"Title"} />
+
             
 
         </form>
